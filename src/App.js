929b523b77bcf871b1a32487541f9866af09d527
Saniago <velasquezsantiago0412@gmail.com>
2022-07-03 12:11:20 -0500

    entregable finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+function App() {
+  const [data, setData] = useState({});
+  const [celcius,setCelcius] = useState(0);
+  const [isFahrenheit,setIsFahrenheit] = useState(true);
+
+  useEffect(() => {
+
+    const success = pos => {
+      const lat = pos.coords.latitude;
+      const lon = pos.coords.longitude;
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7943f41dbd670c537af92d3c756bb622`)
+        .then((res) =>{ 
+          setData(res.data);
+          setCelcius(res.data.main?.temp)
+        });
+    
+      }
+      navigator.geolocation.getCurrentPosition(success);
+    }, [])
+    
+    console.log(data)
+    
+    
+    const convertCelcius = () =>{
+      
+      
+      if (isFahrenheit){
+        setCelcius((celcius -273.15) * (9/5) + 32);
+        setIsFahrenheit(false);
+      } else{
+        setCelcius((celcius -32)*5/9 +273.15 );
+        setIsFahrenheit(true);
+      }
+    }
+    
+  
+      return (
+        <div className="Card">
+          <h1>Weather app</h1>
+          <h3>{data.name},{data.sys?.country}</h3>
+          <div className='flex'>
+          <div className='image'>
+          <img className='img' src={`http://openweathermap.org/img/wn/${data.weather?.[0].icon}@2x.png`} alt="" />
+          </div>
+          <div className='container'>
+          <h3>Pressure: {data.main?.pressure}mb</h3>
+          <h3>Clouds: {data.clouds?.all}%</h3>
+          <h3>Humidity: {data.main?.humidity}g/m³</h3>
+          </div>
+          </div>
+          <li>
+            {isFahrenheit? celcius-273.15:celcius}{ isFahrenheit? "°C":"°F"}
+          </li>
+          <button className='convert' onClick={convertCelcius}>Degrees C°/F°</button>        
+        </div>
+      );
+    }
 export default App;
